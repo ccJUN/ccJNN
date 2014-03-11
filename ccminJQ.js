2293efa52526a5878009fe0ba7035ce5a2e2bef5
wenyi <609359462@qq.com>
2014-03-11 17:02:28 +0800

    min JQ
    以后继续加功能，先写个小小版的

--- ccminJQ.js
+++ ccminJQ.js
@@ -53,12 +53,71 @@ function cc(sall){
 	}
 
 }
+function $(){
+	new cc(sall);
+	return cc(sall);
+}
 cc.prototype.click=function(fn){
 	var i=0;
 	for ( i = 0; i < this.Element.length; i++) {
-		alert(this.Element[i].className);
-		alert('2');
 		handal(this.Element[i],'click',fn);
 	}
+	return this;
 
 };
+cc.prototype.show=function(){
+	var i=0;
+	for (var i = 0; i < this.Element.length; i++) {
+		this.Element[i].style.display='block';
+	}
+	return this;
+}
+cc.prototype.hide=function(){
+	var i=0;
+	for (var i = 0; i < this.Element.length; i++) {
+		this.Element[i].style.display='block';
+	}
+	return this;
+}
+cc.prototype.hover=function(fnover,fnout){
+	var i=0;
+	for (var i = 0; i < this.Element.length; i++) {
+		handal(this.Element[i],'mouseover',fnover);
+		handal(this.Element[i],'mouseover',fnout);
+	}
+	return this;
+}
+cc.prototype.css=function(attr,value){
+	if(arguments.length==2)	//设置样式
+	{
+		var i=0;
+		
+		for(i=0;i<this.elements.length;i++)
+		{
+			this.elements[i].style[attr]=value;
+		}
+	}
+	else	//获取样式
+	{
+		if(typeof attr=='string')
+		{
+		//return this.elements[0].style[attr];
+			return getStyle(this.elements[0], attr);
+		}
+		else
+		{
+			for(i=0;i<this.elements.length;i++)
+			{
+				var k='';
+				
+				for(k in attr)
+				{
+					this.elements[i].style[k]=attr[k];
+				}
+			}
+		}
+	}
+	
+	return this;
+
+}
